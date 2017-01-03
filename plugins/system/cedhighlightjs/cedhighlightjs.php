55513a9244c06f3463c3872ae56b44ba5c3ecdcd
cedric.walter <cedric.walter@pactera.com>
2017-01-03 11:51:32 +0100

    added version 1.2.1-free to master

--- plugins/system/cedhighlightjs/cedhighlightjs.php
+++ plugins/system/cedhighlightjs/cedhighlightjs.php
@@ -2,7 +2,7 @@
 /**
  * @package     CedHighlightjs
  * @subpackage  com_cedhighlightjs
- * http://confluence.galaxiis.com/display/GAL/SOFTWARE+LICENSE+AGREEMENT</license>
+ * http://www.gnu.org/licenses/gpl-3.0.html GNU/GPL 3.0</license>
  * @copyright   Copyright (C) 2013-2016 galaxiis.com All rights reserved.
  * @license     The author and holder of the copyright of the software is Cédric Walter. The licensor and as such issuer of the license and bearer of the
  *              worldwide exclusive usage rights including the rights to reproduce, distribute and make the software available to the public
@@ -18,30 +18,53 @@ jimport('joomla.plugin.plugin');
 class plgSystemCedHighlightJs extends JPlugin
 {
 
-    const HIGHLIGHT_JS = "9.4.0";
+	const HIGHLIGHT_JS = "9.4.0";
 
-    function plgSystemAdd2Home(& $subject, $config)
-    {
-        parent::__construct($subject, $config);
-        $this->loadLanguage();
-    }
+	function plgSystemAdd2Home(& $subject, $config)
+	{
+		parent::__construct($subject, $config);
+		$this->loadLanguage();
+	}
 
-    public function onBeforeRender()
-    {
-        //Do not run in admin area and non HTML  (rss, json, error)
-        $app = JFactory::getApplication();
-        if ($app->isAdmin() || JFactory::getDocument()->getType() !== 'html')
-        {
-            return true;
-        }
-        $document = JFactory::getDocument();
-        
-        $document->addScript("//cdnjs.cloudflare.com/ajax/libs/highlight.js/" . self::HIGHLIGHT_JS . "/highlight.min.js");
+	public function onBeforeRender()
+	{
+		//Do not run in admin area and non HTML  (rss, json, error)
+		$app = JFactory::getApplication();
+		if ($app->isAdmin() || JFactory::getDocument()->getType() !== 'html')
+		{
+			return true;
+		}
+		$document = JFactory::getDocument();
 
-        $skins = $this->params->get('skins', 'default.min.css');
-        $document->addStyleSheet("//cdnjs.cloudflare.com/ajax/libs/highlight.js/" . self::HIGHLIGHT_JS . "/styles/$skins");
+		$document->addScript("//cdnjs.cloudflare.com/ajax/libs/highlight.js/" . self::HIGHLIGHT_JS . "/highlight.min.js");
 
-        $document->addScriptDeclaration("\nhljs.initHighlightingOnLoad();\n");
-    }
+		$skins = $this->params->get('skins', 'default.min.css');
+		$document->addStyleSheet("//cdnjs.cloudflare.com/ajax/libs/highlight.js/" . self::HIGHLIGHT_JS . "/styles/$skins");
+
+		$mode = intval($this->paramss->get('mode', 0));
+
+		if ($mode == 0)
+		{
+			$document->addScriptDeclaration("\nhljs.initHighlightingOnLoad();\n");
+		}
+		if ($mode == 1)
+		{
+			$document->addScriptDeclaration("jQuery(document).ready(function() {
+		        jQuery('pre code').each(function(i, block) {
+			        hljs.highlightBlock(block);
+		        });
+	        });");
+		}
+		if ($mode == 2)
+		{
+			$document->addScriptDeclaration("addEventListener('load', function() {
+				var code = document.querySelector('#code');
+				var worker = new Worker('media/plg_system_highlight/worker.js');
+				worker.onmessage = function(event) { code.innerHTML = event.data; }
+                worker.postMessage(code.textContent);
+				});
+			");
+		}
+	}
 
 }
